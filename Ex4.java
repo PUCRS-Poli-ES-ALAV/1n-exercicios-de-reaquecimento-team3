ed5c12af43b71b8633101f9aa57e66ad95e15542
gustavoburgie <gustavoburgie@gmail.com>
2025-07-30 20:34:42 -0300

    dsjjds

--- Ex4.java
+++ Ex4.java
@@ -1,10 +1,10 @@
 public class Ex4 {
     public static void main(String[] args) {
-        System.out.println(inv_str("obas",4));
+        System.out.println(inv_str("obas"));
     }
 
-    public static String inv_str(String s, int n){
-        if(n<0) return "";
-        return inv_str(s.substring(1) + s.charAt(0),n-1);
+    public static String inv_str(String s){
+        if(s.isEmpty()) return s;
+        return inv_str(s.substring(1)) + s.charAt(0);
     }
 }
