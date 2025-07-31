0b997e6325d94c3f9743e5a470812a3fd6b5c942
gustavoburgie <gustavoburgie@gmail.com>
2025-07-31 19:41:00 -0300

    agorasim

--- Ex6.java
+++ Ex6.java
@@ -1,10 +1,14 @@
 public class Ex6 {
     public static void main(String[] args) {
-        
+        System.out.println(Ackerman(1, 2));
     }
 
     public static int Ackerman(int m, int n){
-        return 0; //o
+        if(m == 0)
+        return n+1;
+        if(n == 0)
+        return Ackerman(m-1,1);
 
+        return Ackerman(m-1,Ackerman(m, n-1));
     }
 }
