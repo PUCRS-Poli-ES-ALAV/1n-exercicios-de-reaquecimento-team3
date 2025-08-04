3d85ec4b0ca9813b43b44f1aadcae49cc5086ee2
gustavoburgie <gustavoburgie@gmail.com>
2025-08-04 19:55:00 -0300

    2 feito

--- Exercicios_Modelagem.java
+++ Exercicios_Modelagem.java
@@ -1,6 +1,7 @@
 public class Exercicios_Modelagem {
     public static void main(String args[]){
         System.out.println(fatorial(3));
+        System.out.println(somatorio(0));
     }   
     public static int fatorial(int n){
         if(n == 0)
@@ -12,8 +13,15 @@ public class Exercicios_Modelagem {
     }
 
     public static int somatorio(int n){
+        if(n > 0)
+        return somatorio(n-1);
+        if(n < 0)
+        return somatorio(n+1);
+        else
         return 0;
     }
+
+    
 
 }
 
