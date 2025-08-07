0fe48bea0ec5f7bca4804868d9017be47b330a26
Gustavo Amaro <gustavamarodasilva@gmail.com>
2025-08-06 21:00:03 -0300

    Fix method name casing for ackerman function in Ex6.java

--- Ex6.java
+++ Ex6.java
@@ -1,14 +1,14 @@
 public class Ex6 {
     public static void main(String[] args) {
-        System.out.println(Ackerman(1, 2));
+        System.out.println(ackerman(1, 2));
     }
 
-    public static int Ackerman(int m, int n){
-        if(m == 0)
-        return n+1;
-        if(n == 0)
-        return Ackerman(m-1,1);
+    public static int ackerman(int m, int n) {
+        if (m == 0)
+            return n + 1;
+        if (n == 0)
+            return ackerman(m - 1, 1);
 
-        return Ackerman(m-1,Ackerman(m, n-1));
+        return ackerman(m - 1, ackerman(m, n - 1));
     }
 }
